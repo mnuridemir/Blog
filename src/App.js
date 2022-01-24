aba38e7f1dc6802b0a536e6aefdbee048d37df17
mnuridemir <mehmetnuridemirr@gmail.com>
2022-01-25 00:30:33 +0300

    router-dom links added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Setting";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { Routes, Route, Link } from "react-router-dom";
+import SinglePost from "./components/singlePost/SinglePost";
 
 
 function App() {
+  const user = false
   return (
     <>
-      
-      <TopBar/>
-      <Register/>
     
+    <TopBar/>
+    <Routes>
+      
+        <Route path="" element={<Home/>} />
+        <Route path="register" element={user ? <Home/> : <Register />} />
+        <Route path="login" element={user ? <Home/> : <Login />} />
+        <Route path="write" element={user ? <Write/> : <Register />} />
+        <Route path="setting" element={user ? <Setting/> : <Register />} />
+        <Route path="post/:postId" element={<SinglePost />} />
+    
+    </Routes>
     </>
   );
 }
